2275b552bf9e20e0acf3e2c0ac4e7e5b0812743d
Phillip Darby <darby.phillip@gmail.com>
2019-08-20 15:19:09 -0400

    Move repos state to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -41,13 +41,20 @@ const GithubState = props => {
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    dispatch({
-      type: GET_USER,
-      payload: res.data
-    });
+    dispatch({ type: GET_USER, payload: res.data });
   };
 
-  // Get Repos
+  //get users repos
+  const getUserRepos = async username => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({ type: GET_REPOS, payload: res.data });
+  };
 
   //clear users
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
@@ -63,7 +70,8 @@ const GithubState = props => {
         loading: state.loading,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos
       }}
     >
       {props.children}
